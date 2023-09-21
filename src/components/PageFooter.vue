873cc79fe76acd24d961cd001fc448f4705d17e8
Dmitriy <rsp.fdrf@yandex.ru>
2023-09-21 20:29:04 +0500

    add footerpage component

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+  <footer>
+    <div class="wrapper-footer">
+      <p>И что же сюда положить?</p>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+
+  footer {
+    margin-top: 50px;
+    box-shadow: 0px 2px 15px 0px #64646F33;
+  }
+  .wrapper-footer {
+  display: block;
+  max-width: 1000px;
+  height: 50px;
+  margin: 0 auto;
+
+  }
+  p {
+    text-align: center;
+    padding-top: 15px;
+  }
+</style>
